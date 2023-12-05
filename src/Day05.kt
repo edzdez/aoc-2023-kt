1f1ca11385acd49fcbdbf601a905dd511478332e
Ethan Zhang <ezsland05@gmail.com>
2023-12-05 02:24:33 -0600

    nvm ig it works lmfao

--- src/Day05.kt
+++ src/Day05.kt
@@ -3,23 +3,23 @@ import kotlin.math.min
 
 fun main() {
     data class Almanac(
-        val seeds: List<ULong>, val maps: List<MutableMap<Pair<ULong, ULong>, ULong>>
+        val seeds: List<Long>, val maps: List<MutableMap<Pair<Long, Long>, Long>>
     )
 
     fun parseInput1(input: List<String>): Almanac {
-        val seeds = input[0].substringAfter("seeds: ").split(" ").map(String::toULong).toList()
-        val maps = mutableListOf<MutableMap<Pair<ULong, ULong>, ULong>>()
+        val seeds = input[0].substringAfter("seeds: ").split(" ").map(String::toLong).toList()
+        val maps = mutableListOf<MutableMap<Pair<Long, Long>, Long>>()
 
         val it = input.drop(2).listIterator()
         while (it.hasNext()) {
             it.next()
-            val map = mutableMapOf<Pair<ULong, ULong>, ULong>()
+            val map = mutableMapOf<Pair<Long, Long>, Long>()
             while (it.hasNext()) {
                 val line = it.next()
                 if (line.isBlank()) break
 
-                val nums = line.split(' ').map(String::toULong)
-                map[Pair(nums[1], nums[1] + nums[2] - 1UL)] = nums[0]
+                val nums = line.split(' ').map(String::toLong)
+                map[Pair(nums[1], nums[1] + nums[2] - 1L)] = nums[0]
             }
 
             maps.add(map)
@@ -28,7 +28,7 @@ fun main() {
         return Almanac(seeds, maps)
     }
 
-    fun findInMap(map: MutableMap<Pair<ULong, ULong>, ULong>, num: ULong): ULong {
+    fun findInMap(map: MutableMap<Pair<Long, Long>, Long>, num: Long): Long {
         val a = map.keys.filter { num >= it.first && num <= it.second }
         if (a.isEmpty()) return num
         val startRange = a.first()
@@ -36,7 +36,7 @@ fun main() {
         return end + (num - startRange.first)
     }
 
-    fun joinRanges(ranges: List<Pair<ULong, ULong>>): List<Pair<ULong, ULong>> {
+    fun joinRanges(ranges: List<Pair<Long, Long>>): List<Pair<Long, Long>> {
         val retRanges = mutableListOf(ranges[0])
         for (range in ranges) {
             if (range.second <= retRanges.last().second) {
@@ -52,7 +52,7 @@ fun main() {
         return retRanges
     }
 
-    fun findIntersection(range: Pair<ULong, ULong>, from: Pair<ULong, ULong>): Pair<ULong, ULong>? {
+    fun findIntersection(range: Pair<Long, Long>, from: Pair<Long, Long>): Pair<Long, Long>? {
         return if (range.first <= from.first && range.second >= from.second) from
         else if (from.first <= range.first && from.second >= range.second) range
         else if ((from.first <= range.second && range.second <= from.second) || (range.first <= from.second && from.second <= range.second)) Pair(
@@ -66,9 +66,9 @@ fun main() {
         else null
     }
 
-    fun mapRange(map: MutableMap<Pair<ULong, ULong>, ULong>, range: Pair<ULong, ULong>): List<Pair<ULong, ULong>> {
-        val satisfiedRange = mutableListOf<Pair<ULong, ULong>>()
-        val actualRange = mutableListOf<Pair<ULong, ULong>>()
+    fun mapRange(map: MutableMap<Pair<Long, Long>, Long>, range: Pair<Long, Long>): List<Pair<Long, Long>> {
+        val satisfiedRange = mutableListOf<Pair<Long, Long>>()
+        val actualRange = mutableListOf<Pair<Long, Long>>()
         for ((fromRange, to) in map) {
             val intersection = findIntersection(range, fromRange)
             if (intersection != null) {
@@ -82,28 +82,21 @@ fun main() {
             }
         }
 
-        if (satisfiedRange.isEmpty()) {
-            actualRange.add(range)
-            return actualRange
-        }
-
+        satisfiedRange.add(Pair(range.first - 1, range.first - 1))
+        satisfiedRange.add(Pair(range.second + 1, range.second + 1))
         satisfiedRange.sortBy { it.first }
-//        println("sat $satisfiedRange")
-        for ((r1, r2) in satisfiedRange.windowed(2, 1)) {
-            actualRange.add(Pair(r1.second + 1UL, r2.second - 1UL))
-        }
-        val first = satisfiedRange.firstOrNull()
-        if (first != null && first.first > range.first) actualRange.add(Pair(range.first, first.first - 1UL))
-        val last = satisfiedRange.lastOrNull()
-        if (last != null && last.second < range.second) actualRange.add(Pair(last.second + 1UL, range.second))
 
+        for ((a, b) in satisfiedRange.windowed(2, 1)) {
+            if (a.second + 1 != b.first)
+                actualRange.add(Pair(a.second + 1, b.first - 1))
+        }
         return actualRange
     }
 
-    fun part1(input: List<String>): ULong {
+    fun part1(input: List<String>): Long {
         val parsed = parseInput1(input)
 
-        var locNumber = ULong.MAX_VALUE
+        var locNumber = Long.MAX_VALUE
         for (seed in parsed.seeds) {
             var num = seed
             for (map in parsed.maps) {
@@ -115,14 +108,14 @@ fun main() {
         return locNumber
     }
 
-    fun part2(input: List<String>): ULong {
+    fun part2(input: List<String>): Long {
         val parsed = parseInput1(input)
-        val initialRanges = parsed.seeds.chunked(2).map { Pair(it[0], it[0] + it[1] - 1UL) }.sortedBy { it.first }
+        val initialRanges = parsed.seeds.chunked(2).map { Pair(it[0], it[0] + it[1] - 1L) }.sortedBy { it.first }
         var joinedRanges = joinRanges(initialRanges)
 //        println("ranges: $joinedRanges")
 
         for (map in parsed.maps) {
-            val newRanges = mutableListOf<Pair<ULong, ULong>>()
+            val newRanges = mutableListOf<Pair<Long, Long>>()
             for (range in joinedRanges) {
                 val add = mapRange(map, range)
                 newRanges.addAll(add)
@@ -135,12 +128,12 @@ fun main() {
 
         // works for my input, probably doesn't work for anyone else :)
         // in fact, i tested it. It does not work :)
-        return joinedRanges.filter { it.first != 0UL }.minOfOrNull { it.first }!!
+        return joinedRanges.first().first
     }
 
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 35UL)
-    check(part2(testInput) == 46UL)
+    check(part1(testInput) == 35L)
+    check(part2(testInput) == 46L)
 
     val input = readInput("Day05")
     part1(input).println()
